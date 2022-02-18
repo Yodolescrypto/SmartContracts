63999caf80db18192c64d5ef40b6f448b028246f
Yodo <yodo@DESKTOP-A0UPJI7.localdomain>
2022-02-18 14:01:41 +0100

    sniping

--- ERC20/snipe.py
+++ ERC20/snipe.py
@@ -9,7 +9,7 @@ web3 = Web3(Web3.HTTPProvider(infura_url))
 
 # uniswap address and abi
 uniswap_router = '0x60aE616a2155Ee3d9A68541Ba4544862310933d4'
-uniswap_factory = '0x9ad6c38be94206ca50bb0d90783181662f0cfa10'
+uniswap_factory = Web3.toChecksumAddress('0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10')
 uniswap_factory_abi = json.loads('[{"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token0","type":"address"},{"indexed":true,"internalType":"address","name":"token1","type":"address"},{"indexed":false,"internalType":"address","name":"pair","type":"address"},{"indexed":false,"internalType":"uint256","name":"","type":"uint256"}],"name":"PairCreated","type":"event"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"allPairs","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"allPairsLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"}],"name":"createPair","outputs":[{"internalType":"address","name":"pair","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"feeTo","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"feeToSetter","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeTo","type":"address"}],"name":"setFeeTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"name":"setFeeToSetter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
 
 contract = web3.eth.contract(address=uniswap_factory, abi=uniswap_factory_abi)
@@ -19,7 +19,7 @@ contract = web3.eth.contract(address=uniswap_factory, abi=uniswap_factory_abi)
 def handle_event(event):
     print(Web3.toJSON(event))
     # and whatever
-
+    
 
 # asynchronous defined function to loop
 # this loop sets up an event filter and is looking for new entires for the "PairCreated" event
